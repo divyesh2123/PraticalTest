dac69f74576a7afd3d069ff960e0108e84295906
lenovo <lenovo@LAPTOP-BLNTEBH7>
2022-06-03 11:38:19 +0530

    This is data

--- praticaltest/src/store/store.js
+++ praticaltest/src/store/store.js
@@ -3,13 +3,19 @@ import rootReducer from "../reducer";
 import counterReducer from "../reducer/counterReducer";
 import thunk from 'redux-thunk';
 import { applyMiddleware } from "redux";
+import createSagaMiddleware from 'redux-saga'
+import rootSaga from "../sagas/sagas";
 
+
+const sagaMiddleware = createSagaMiddleware()
 
 const store = createStore(
   rootReducer,
-  applyMiddleware(thunk)
+  applyMiddleware(sagaMiddleware)
 );
 
+
+sagaMiddleware.run(rootSaga);
 
 
   export default store;
